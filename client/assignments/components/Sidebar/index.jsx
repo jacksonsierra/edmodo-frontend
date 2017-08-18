2741dcd0cee083a91dec44ce8b5d5337f8d3e856
Jackson Sierra <jks718@gmail.com>
2017-08-17 22:51:07 -0700

    [feature] add assignments sidebar

--- client/assignments/components/Sidebar/index.jsx
+++ client/assignments/components/Sidebar/index.jsx
@@ -1,5 +1,27 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import SidebarItem from 'client/assignments/containers/SidebarItem'
 import styles from './styles.css'
 
-export default class Sidebar extends Component {}
+export default class Sidebar extends Component {
+  static propTypes = {
+    assignments: PropTypes.arrayOf(PropTypes.object),
+  }
+
+  render() {
+    const {
+      assignments,
+    } = this.props
+
+    return (
+      <div className={styles.container}>
+        {assignments.map((assignment) => (
+          <SidebarItem
+            key={assignment.id}
+            {...assignment}
+          />
+        ))}
+      </div>
+    )
+  }
+}
--- client/assignments/components/SidebarItem/index.jsx
+++ client/assignments/components/SidebarItem/index.jsx
@@ -1,5 +1,30 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router'
+import moment from 'moment'
 import styles from './styles.css'
 
-export default class SidebarItem extends Component {}
+export default class SidebarItem extends Component {
+  static propTypes = {
+    id: PropTypes.number,
+    title: PropTypes.string,
+    due_at: PropTypes.string,
+  }
+
+  render() {
+    const {
+      id,
+      title,
+      due_at,
+    } = this.props
+
+    return (
+      <div className={styles.container}>
+        <Link to={id}>
+          <span className={styles.title}>{title}</span>
+          <span className={styles.date}>{`due ${moment(due_at)}`}</span>
+        </Link>
+      </div>
+    )
+  }
+}
