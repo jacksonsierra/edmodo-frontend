dac8bb19303edf98b2dd71920e4c789a3370aa46
Jackson Sierra <jks718@gmail.com>
2017-08-17 21:27:09 -0700

    [feature] add main assignments component

---
+++ client/assignments/components/BlankAssignment/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './styles.css'
+
+export default function BlankAssignment() {
+  return (
+    <div className={styles.container}>
+      <span>Select an assignment</span>
+    </div>
+  )
+}
--- client/assignments/containers/Assignments/index.jsx
+++ client/assignments/containers/Assignments/index.jsx
@@ -1,11 +1,47 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import BlankAssignment from 'client/assignments/components/BlankAssignment'
+import Sidebar from 'client/assignments/components/Sidebar'
+import { getAssignmentsList } from 'client/assignments/actions/assignments'
 import styles from './styles.css'
 
-const mapStateToProps = (state) => ({})
+const mapStateToProps = (state) => state
 
-const mapDispatchToProps = {}
+const mapDispatchToProps = {
+  getAssignmentsList,
+}
 
 @connect(mapStateToProps, mapDispatchToProps)
-export default class Assignments extends Component {}
+export default class Assignments extends Component {
+  static propTypes = {
+    children: PropTypes.node,
+    assignments: PropTypes.object,
+    getAssignmentsList: PropTypes.func,
+  }
+
+  componentDidMount() {
+    const {
+      page,
+      per_page,
+    } = this.props.assignments
+    this.props.getAssignmentsList({ page, per_page })
+  }
+
+  render() {
+    const {
+      assignments,
+      children,
+    } = this.props
+
+    return (
+      <div className={styles.container}>
+        <h1 className={styles.title}>Assignments</h1>
+        <div className={styles.body}>
+          <Sidebar {...assignments} />
+          {children || <BlankAssignment />}
+        </div>
+      </div>
+    )
+  }
+}
