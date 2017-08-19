411735e531e2bc05bfef6f9ee9833e41aa050a16
Jackson Sierra <jks718@gmail.com>
2017-08-19 15:02:38 -0700

    [feature] add individual submission component

--- client/assignments/components/Submission/index.jsx
+++ client/assignments/components/Submission/index.jsx
@@ -1,5 +1,75 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import {
+  Arrow,
+  ARROW_DIRECTIONS,
+} from 'client/shared/components/Arrow'
+import { formatDate } from 'client/utils'
 import styles from './styles.css'
 
-export default class Submission extends Component {}
+export default class Submission extends Component {
+  static propTypes = {
+    id: PropTypes.number,
+    creator: PropTypes.object,
+    content: PropTypes.string,
+    submitted_at: PropTypes.string,
+  }
+
+  constructor() {
+    super()
+
+    this.state = {
+      showContent: false,
+    }
+  }
+
+  render() {
+    const {
+      creator,
+      content,
+      submitted_at,
+    } = this.props
+    const { showContent } = this.state
+    const submittedAt = formatDate(submitted_at)
+
+    return (
+      <div className={styles.container}>
+        <div className={styles.row}>
+          <img
+            className={styles.avatar}
+            src={creator.avatars.large}
+          />
+          <div className={styles.text}>
+            <h2 className={styles.creator}>
+              {`${creator.first_name} ${creator.last_name}`}
+            </h2>
+            <span className={styles.date}>{submittedAt}</span>
+          </div>
+          <Arrow
+            onClick={this.onClickArrow}
+            direction={this.getArrowDirection()}
+          />
+        </div>
+        {showContent && (
+          <div className={styles.content}>
+            <p>{content}</p>
+          </div>
+        )}
+      </div>
+    )
+  }
+
+  onClickArrow = () => {
+    this.setState({
+      showContent: !this.state.showContent,
+    })
+  }
+
+  getArrowDirection = () => {
+    if (this.state.showContent) {
+      return ARROW_DIRECTIONS.UP
+    }
+
+    return ARROW_DIRECTIONS.DOWN
+  }
+}
---
+++ client/shared/components/Arrow/index.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import styles from './styles.css'
+
+export const ARROW_DIRECTIONS = Object.freeze({
+  UP: 'up',
+  DOWN: 'down',
+  LEFT: 'left',
+  RIGHT: 'right',
+})
+
+export class Arrow extends Component {
+  static propTypes = {
+    onClick: PropTypes.func,
+    direction: PropTypes.string,
+  }
+
+  static defaultProps = {
+    direction: ARROW_DIRECTIONS.DOWN,
+  }
+
+  render() {
+    const className = this.getClassName()
+
+    return (
+      <div
+        className={`${styles.container} ${className}`}
+        onClick={this.props.onClick}
+      />
+    )
+  }
+
+  getClassName = () => {
+    const { direction } = this.props
+
+    switch (direction) {
+      case ARROW_DIRECTIONS.UP:
+        return styles.up
+      case ARROW_DIRECTIONS.LEFT:
+        return styles.left
+      case ARROW_DIRECTIONS.RIGHT:
+        return styles.right
+      default:
+        return styles.down
+    }
+  }
+}
